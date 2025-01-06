a18b0e0c6baea3924742391b7c9bbbb00aafa1c4
xeniaagis <20230910058@uniku.ac.id>
2025-01-06 12:22:57 +0700

    Add files via upload

---
+++ MODUL_08/Latihan_08_menu.php
@@ -0,0 +1,11 @@
+<aside class="col-md-2 p-0">
+
+<nav class="nav flex-column bg-light p-3 m-0">
+
+<a class="nav-link active" href="?menu=a">Latihan 8a</a>
+<a class="nav-link active" href="?menu=b">Latihan 8b</a>
+<a class="nav-link active" href="?menu=c">Latihan 8c</a>
+<a class="nav-link active" href="?menu=d">Latihan 8d</a>
+</nav>
+
+</aside>
